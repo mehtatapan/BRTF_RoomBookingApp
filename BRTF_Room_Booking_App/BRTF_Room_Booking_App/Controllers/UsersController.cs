96a7c38d9203fdaf8e730655f7788451e391fd69
jessterryberry <jlterryb@gmail.com>
2022-02-12 19:53:51 -0500

    Half finished .csv upload

--- BRTF_Room_Booking_App/BRTF_Room_Booking_App/Controllers/UsersController.cs
+++ BRTF_Room_Booking_App/BRTF_Room_Booking_App/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using BRTF_Room_Booking_App.Utilities;
 using Microsoft.AspNetCore.Http;
 using OfficeOpenXml;
 using System.IO;
+using System.Text;
 
 namespace BRTF_Room_Booking_App.Controllers
 {
@@ -320,15 +321,18 @@ namespace BRTF_Room_Booking_App.Controllers
 
             try
             {
-                ExcelPackage excel;
-                using (var memoryStream = new MemoryStream())
+                //ExcelPackage excel;
+                var csvExcel = new StringBuilder();
+                using (var reader = new StreamReader(theExcel.OpenReadStream()))
                 {
-                    await theExcel.CopyToAsync(memoryStream);
-                    excel = new ExcelPackage(memoryStream);
+                    while (reader.Peek() >= 0)
+                        csvExcel.AppendLine(reader.ReadLine());
                 }
-                var workSheet = excel.Workbook.Worksheets[0];
-                var start = workSheet.Dimension.Start;
-                var end = workSheet.Dimension.End;
+
+                string excel = csvExcel.ToString();
+                //var workSheet = excel.Workbook.Worksheets[0];
+                //var start = workSheet.Dimension.Start;
+                //var end = workSheet.Dimension.End;
 
                 //check for duplicates
                 var existingUsers = (_context.Users
@@ -339,81 +343,167 @@ namespace BRTF_Room_Booking_App.Controllers
                 //Start a new list to hold imported objects
                 List<User> users = new List<User>();
 
-                for (int row = start.Row + 1; row <= end.Row; row++)
+                //Read the contents of the CSV file
+                //string csvData = System.IO.File.ReadAllText(filePath);
+
+                int firow = 1;
+
+                //Execute a loop over the rows
+                foreach (string row in excel.Split("\r\n"))
                 {
-                    //Look for Term and Program or create it if it doesnt exist
-                    //Query all Terms and Programs
-                    var programs = from p in _context.TermAndPrograms
-                                   select p;
-
-                    //Check if we have the specific combination of Program Code and Level already
-                    programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(workSheet.Cells[row, 5].Text.ToUpper())
-                                                && p.ProgramLevel.Equals(int.Parse(workSheet.Cells[row, 8].Text)));
-                    int progID;
-
-
-                    if (programs.Count() > 0)
+                    if (firow == 1)
                     {
-                        //If we have matches, we grab the ID (FirstOrDefault as there should only be one result) so we can assign it on the User
-                        progID = programs.FirstOrDefault().ID;
+                        firow++;
+                        continue;
                     }
-                    else
+                    if (!string.IsNullOrEmpty(row))
                     {
-                        //If there are no matches, we create a new TermAndProgram object from the CSV file and add it to the context
-                        TermAndProgram prog = new TermAndProgram
                         {
-                            ProgramCode = workSheet.Cells[row, 5].Text,
-                            ProgramName = workSheet.Cells[row, 6].Text,
-                            ProgramLevel = int.Parse(workSheet.Cells[row, 8].Text),
-                            UserGroupID = 1
-                        };
-                        _context.TermAndPrograms.Add(prog);
-                        _context.SaveChanges();
+                            //Look for Term and Program or create it if it doesnt exist
+                            //Query all Terms and Programs
+                            var programs = from p in _context.TermAndPrograms
+                                           select p;
 
-                        //Re-query the programs the same way so we can get the ID of the newly added Term and Program
-                        programs = from p in _context.TermAndPrograms
-                                   select p;
+                            string progCode = row.Split("\t")[5].ToUpper();
+                            int progLevel = Convert.ToInt32(row.Split("\t")[8]);
 
-                        programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(workSheet.Cells[row, 5].Text.ToUpper())
-                                                    && p.ProgramLevel.Equals(int.Parse(workSheet.Cells[row, 8].Text)));
+                            //Check if we have the specific combination of Program Code and Level already
+                            programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(progCode)
+                                                        && p.ProgramLevel.Equals(progLevel));
+                            int progID;
 
-                        progID = programs.FirstOrDefault().ID;
+
+                            if (programs.Count() > 0)
+                            {
+                                //If we have matches, we grab the ID (FirstOrDefault as there should only be one result) so we can assign it on the User
+                                progID = programs.FirstOrDefault().ID;
+                            }
+                            else
+                            {
+                                //If there are no matches, we create a new TermAndProgram object from the CSV file and add it to the context
+                                TermAndProgram prog = new TermAndProgram
+                                {
+                                    ProgramCode = row.Split("\t")[5],
+                                    ProgramName = row.Split("\t")[6],
+                                    ProgramLevel = int.Parse(row.Split("\t")[8]),
+                                    UserGroupID = 1
+                                };
+                                _context.TermAndPrograms.Add(prog);
+                                _context.SaveChanges();
+
+                                //Re-query the programs the same way so we can get the ID of the newly added Term and Program
+                                programs = from p in _context.TermAndPrograms
+                                           select p;
+
+                                programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(progCode)
+                                                            && p.ProgramLevel.Equals(progLevel));
+
+                                progID = programs.FirstOrDefault().ID;
+                            }
+
+                            //Row by row...
+                            User u = new User
+                            {
+                                FirstName = row.Split("\t")[2],
+                                MiddleName = row.Split("\t")[3],
+                                LastName = row.Split("\t")[4],
+                                TermAndProgramID = progID,
+                                Email = row.Split("\t")[7],
+                                EmailBookingNotifications = true,
+                                EmailCancelNotifications = true,
+                                RoleID = 2,
+                                Username = row.Split("\t")[1],
+                                Password = "password"
+                            };
+                            //count the duplicates
+                            if (existingUsers.Contains(u.Email))
+                            {
+                                j++;
+                            }
+                            else
+                            {
+                                users.Add(u);
+                                i++;
+                            }
+                        }
                     }
 
-                    // Row by row...
-                    User u = new User
-                    {
-                        //  ID = int.Parse(workSheet.Cells[row, 1].Text),
-                        FirstName = workSheet.Cells[row, 2].Text,
-                        MiddleName = workSheet.Cells[row, 3].Text,
-                        LastName = workSheet.Cells[row, 4].Text,
-                        TermAndProgramID = progID,
-                        Email = workSheet.Cells[row, 7].Text,
-                        EmailBookingNotifications = true,
-                        EmailCancelNotifications = true,
-                        RoleID = 2,
-                        Username = workSheet.Cells[row, 1].Text,
-                        Password = "password"
-                    };
-                     //count the duplicates
-                    if (existingUsers.Contains(u.Email))
-                    {
-                        j++;
-                    }
-                    else
-                    {
-                        users.Add(u);
-                        i++;
-                    }
+                    //for (int row = start.Row + 1; row <= end.Row; row++)
+                    //{
+                    //    //Look for Term and Program or create it if it doesnt exist
+                    //    //Query all Terms and Programs
+                    //    var programs = from p in _context.TermAndPrograms
+                    //                   select p;
+
+                    //    //Check if we have the specific combination of Program Code and Level already
+                    //    programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(workSheet.Cells[row, 5].Text.ToUpper())
+                    //                                && p.ProgramLevel.Equals(int.Parse(workSheet.Cells[row, 8].Text)));
+                    //    int progID;
+
+
+                    //    if (programs.Count() > 0)
+                    //    {
+                    //        //If we have matches, we grab the ID (FirstOrDefault as there should only be one result) so we can assign it on the User
+                    //        progID = programs.FirstOrDefault().ID;
+                    //    }
+                    //    else
+                    //    {
+                    //        //If there are no matches, we create a new TermAndProgram object from the CSV file and add it to the context
+                    //        TermAndProgram prog = new TermAndProgram
+                    //        {
+                    //            ProgramCode = workSheet.Cells[row, 5].Text,
+                    //            ProgramName = workSheet.Cells[row, 6].Text,
+                    //            ProgramLevel = int.Parse(workSheet.Cells[row, 8].Text),
+                    //            UserGroupID = 1
+                    //        };
+                    //        _context.TermAndPrograms.Add(prog);
+                    //        _context.SaveChanges();
+
+                    //        //Re-query the programs the same way so we can get the ID of the newly added Term and Program
+                    //        programs = from p in _context.TermAndPrograms
+                    //                   select p;
+
+                    //        programs = programs.Where(p => p.ProgramCode.ToUpper().Equals(workSheet.Cells[row, 5].Text.ToUpper())
+                    //                                    && p.ProgramLevel.Equals(int.Parse(workSheet.Cells[row, 8].Text)));
+
+                    //        progID = programs.FirstOrDefault().ID;
+                    //    }
+
+                    //    // Row by row...
+                    //    User u = new User
+                    //    {
+                    //        //  ID = int.Parse(workSheet.Cells[row, 1].Text),
+                    //        FirstName = workSheet.Cells[row, 2].Text,
+                    //        MiddleName = workSheet.Cells[row, 3].Text,
+                    //        LastName = workSheet.Cells[row, 4].Text,
+                    //        TermAndProgramID = progID,
+                    //        Email = workSheet.Cells[row, 7].Text,
+                    //        EmailBookingNotifications = true,
+                    //        EmailCancelNotifications = true,
+                    //        RoleID = 2,
+                    //        Username = workSheet.Cells[row, 1].Text,
+                    //        Password = "password"
+                    //    };
+                    //     //count the duplicates
+                    //    if (existingUsers.Contains(u.Email))
+                    //    {
+                    //        j++;
+                    //    }
+                    //    else
+                    //    {
+                    //        users.Add(u);
+                    //        i++;
+                    //    }
+                    //}
+                    uploadMessage = "There were " + j + " duplicate(s). Total Users created: " + i;
+                    _context.Users.AddRange(users);
+                    _context.SaveChanges();
                 }
-                uploadMessage = "There were " + j + " duplicate(s). Total Users created: " + i;
-                _context.Users.AddRange(users);
-                _context.SaveChanges();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.GetBaseException().Message);
-              //  uploadMessage = "Failed to import data.  Check that you selected the correct file in the correct format.";
+                //  uploadMessage = "Failed to import data.  Check that you selected the correct file in the correct format.";
             }
             TempData["Message"] = uploadMessage;
             return RedirectToAction("Index");
